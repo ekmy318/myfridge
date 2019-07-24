83fa7848211c6acf1674a3fc751474d9e4032171
ekmy318 <eunice.xp@gmail.com>
2019-07-24 14:11:48 -0400

    update config url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://thawing-beyond-81012.herokuapp.com/',
+  production: 'https://thawing-beyond-81012.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
